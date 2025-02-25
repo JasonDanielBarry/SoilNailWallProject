6412c76a0779b32502ca09996405fa372890360f
JasonDanielBarry <jasondanielbarry512@gmail.com>
2025-02-25 13:44:25 +0200

    (1) working on save file

--- Source/SoilNailWallTypes/SoilNailWallFileReaderWriterClass.pas
+++ Source/SoilNailWallTypes/SoilNailWallFileReaderWriterClass.pas
@@ -40,22 +40,37 @@ implementation
 
         //read
             function TSoilNailWallFileReaderWriter.tryReadLimitStateMaterial(const identifierIn : string; out valueOut : TLimitStateMaterial) : boolean;
+                var
+                    aveVal : string;
+                    readXMLNode : IXMLNode;
                 begin
+
+                    aveVal := tryRead( readXMLNode.ChildNodes.FindNode('AverageValue').text );
+
+
 
                 end;
 
         //write
             procedure TSoilNailWallFileReaderWriter.writeLimitStateMaterial(const identifierIn : string; const valueIn : TlimitStateMaterial);
                 var
+                    newXMLNode : IXMLNode;
                     limitStateValuesArray : TArray<double>;
                 begin
                     //place data into a double array
                         SetLength( limitStateValuesArray, 4 );
 
-                        limitStateValuesArray[0] := valueIn.averageValue;
-                        limitStateValuesArray[1] := valueIn.variationCoefficient;
-                        limitStateValuesArray[2] := valueIn.downgradeFactor;
-                        limitStateValuesArray[3] := valueIn.partialFactor;
+//                        limitStateValuesArray[0] := valueIn.averageValue;
+//                        limitStateValuesArray[1] := valueIn.variationCoefficient;
+//                        limitStateValuesArray[2] := valueIn.downgradeFactor;
+//                        limitStateValuesArray[3] := valueIn.partialFactor;
+
+
+                    newXMLNode := createNewNode( identifierIn, 'LIMITSTATETYPE' );
+
+                    newXMLNode.AddChild('AverageValue').text := FloatToStr( valueIn.averageValue );
+                    newXMLNode.AddChild('VariationCoeff').text := FloatToStr( valueIn.variationCoefficient );
+
 
                     //write double array
                         writeDoubleArray( identifierIn, limitStateValuesArray );
